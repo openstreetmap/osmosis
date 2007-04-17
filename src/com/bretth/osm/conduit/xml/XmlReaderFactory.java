8199e9206eb4a855f678f9c5ff803508b969de7d
Brett Henderson <brett@bretth.com>
2007-04-17 09:15:55 +0000

    Modified XML reader and writer factories to load command line arguments.

--- src/com/bretth/osm/conduit/xml/XmlReaderFactory.java
+++ src/com/bretth/osm/conduit/xml/XmlReaderFactory.java
@@ -1,5 +1,6 @@
 package com.bretth.osm.conduit.xml;
 
+import java.io.File;
 import java.util.Map;
 
 import com.bretth.osm.conduit.pipeline.OsmSourceManager;
@@ -9,10 +10,30 @@ import com.bretth.osm.conduit.pipeline.TaskManagerFactory;
 
 public class XmlReaderFactory extends TaskManagerFactory {
 	private static final String TASK_TYPE = "read-osm";
+	private static final String ARG_FILE_NAME = "file";
+	private static final String DEFAULT_FILE_NAME = "dump.osm";
 	
 	
 	protected TaskManager createTaskManagerImpl(Map<String, String> taskArgs, Map<String, String> pipeArgs) {
-		return new OsmSourceManager(TASK_TYPE, new XmlReader(), pipeArgs);
+		String fileName;
+		File file;
+		XmlReader task;
+		
+		// Get the task arguments.
+		if (taskArgs.containsKey(ARG_FILE_NAME)) {
+			fileName = taskArgs.get(ARG_FILE_NAME);
+		} else {
+			fileName = DEFAULT_FILE_NAME;
+		}
+		
+		// Create a file object from the file name provided.
+		file = new File(fileName);
+		
+		// Build the task object.
+		task = new XmlReader();
+		task.setFile(file);
+		
+		return new OsmSourceManager(TASK_TYPE, task, pipeArgs);
 	}
 	
 	
--- src/com/bretth/osm/conduit/xml/XmlWriterFactory.java
+++ src/com/bretth/osm/conduit/xml/XmlWriterFactory.java
@@ -1,5 +1,6 @@
 package com.bretth.osm.conduit.xml;
 
+import java.io.File;
 import java.util.Map;
 
 import com.bretth.osm.conduit.pipeline.OsmSinkManager;
@@ -9,10 +10,30 @@ import com.bretth.osm.conduit.pipeline.TaskManagerFactory;
 
 public class XmlWriterFactory extends TaskManagerFactory {
 	private static final String TASK_TYPE = "write-osm";
+	private static final String ARG_FILE_NAME = "file";
+	private static final String DEFAULT_FILE_NAME = "dump.osm";
 	
 	
 	protected TaskManager createTaskManagerImpl(Map<String, String> taskArgs, Map<String, String> pipeArgs) {
-		return new OsmSinkManager(TASK_TYPE, new XmlWriter(), pipeArgs);
+		String fileName;
+		File file;
+		XmlWriter task;
+		
+		// Get the task arguments.
+		if (taskArgs.containsKey(ARG_FILE_NAME)) {
+			fileName = taskArgs.get(ARG_FILE_NAME);
+		} else {
+			fileName = DEFAULT_FILE_NAME;
+		}
+		
+		// Create a file object from the file name provided.
+		file = new File(fileName);
+		
+		// Build the task object.
+		task = new XmlWriter();
+		task.setFile(file);
+		
+		return new OsmSinkManager(TASK_TYPE, task, pipeArgs);
 	}
 	
 	
