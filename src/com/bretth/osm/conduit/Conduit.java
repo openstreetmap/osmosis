0034d8d2edab3bef03f485642a1eaeaf012e5e5a
Brett Henderson <brett@bretth.com>
2007-04-17 09:27:26 +0000

    Removed all old test code and replaced with real calls to the pipeline.

--- src/com/bretth/osm/conduit/Conduit.java
+++ src/com/bretth/osm/conduit/Conduit.java
@@ -1,14 +1,9 @@
 package com.bretth.osm.conduit; 
 
-import java.io.File;
-
-import com.bretth.osm.conduit.mysql.DatabaseReader;
 import com.bretth.osm.conduit.mysql.DatabaseReaderFactory;
-import com.bretth.osm.conduit.mysql.DatabaseWriter;
 import com.bretth.osm.conduit.mysql.DatabaseWriterFactory;
-import com.bretth.osm.conduit.xml.XmlReader;
+import com.bretth.osm.conduit.pipeline.Pipeline;
 import com.bretth.osm.conduit.xml.XmlReaderFactory;
-import com.bretth.osm.conduit.xml.XmlWriter;
 import com.bretth.osm.conduit.xml.XmlWriterFactory;
 
 
@@ -17,12 +12,15 @@ public class Conduit {
 	public static void main(String[] args) {
 		registerTasks();
 		
-		if (false) {
-			doLoad();
-		}
-		if (true) {
-			doDump();
-		}
+		Pipeline pipeline;
+		
+		pipeline = new Pipeline();
+		
+		pipeline.prepare(args);
+		
+		pipeline.run();
+		
+		pipeline.waitForCompletion();
 	}
 	
 	
@@ -31,35 +29,5 @@ public class Conduit {
 		new DatabaseWriterFactory();
 		new XmlReaderFactory();
 		new XmlWriterFactory();
-	}
-	
-	
-	private static void doLoad() {
-		File file;
-		XmlReader loader;
-		DatabaseWriter databaseWriter;
-		
-		//file = new File("/home/brett/work/osm/josm/Melbourne.osm");
-		file = new File("/home/brett/tmp/planet-070321-utf8.osm");
-		
-		databaseWriter = new DatabaseWriter("localhost", "osm2", "osm", "14brett13");
-		loader = new XmlReader(databaseWriter);
-		loader.setFile(file);
-		
-		loader.run();
-	}
-	
-	
-	private static void doDump() {
-		File file;
-		XmlWriter xmlWriter;
-		DatabaseReader databaseReader;
-		
-		file = new File("/home/brett/tmp/dbdump.osm");
-		
-		xmlWriter = new XmlWriter(file);
-		databaseReader = new DatabaseReader(xmlWriter);
-		
-		databaseReader.run();
 	}
 }
