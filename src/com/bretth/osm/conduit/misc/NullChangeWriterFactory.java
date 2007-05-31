288b92c7b24824900d65aae0ca2893a4ab7f5efc
Brett Henderson <brett@bretth.com>
2007-05-31 09:02:57 +0000

    Add null writer classes allowing data to be discarded for benchmarking purposes.

---
+++ src/com/bretth/osm/conduit/misc/NullChangeWriterFactory.java
@@ -0,0 +1,35 @@
+package com.bretth.osm.conduit.misc;
+
+import java.util.Map;
+
+import com.bretth.osm.conduit.pipeline.ChangeSinkManager;
+import com.bretth.osm.conduit.pipeline.TaskManager;
+import com.bretth.osm.conduit.pipeline.TaskManagerFactory;
+
+
+/**
+ * The task manager factory for a null change writer.
+ * 
+ * @author Brett Henderson
+ */
+public class NullChangeWriterFactory extends TaskManagerFactory {
+	
+	/**
+	 * Creates a new instance and adds the class to the global register.
+	 * 
+	 * @param taskType
+	 *            The name to register the type against.
+	 */
+	public NullChangeWriterFactory(String taskType) {
+		super(taskType);
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected TaskManager createTaskManagerImpl(String taskId, Map<String, String> taskArgs, Map<String, String> pipeArgs) {
+		return new ChangeSinkManager(taskId, new NullChangeWriter(), pipeArgs);
+	}
+}
---
+++ src/com/bretth/osm/conduit/misc/NullWriterFactory.java
@@ -0,0 +1,35 @@
+package com.bretth.osm.conduit.misc;
+
+import java.util.Map;
+
+import com.bretth.osm.conduit.pipeline.SinkManager;
+import com.bretth.osm.conduit.pipeline.TaskManager;
+import com.bretth.osm.conduit.pipeline.TaskManagerFactory;
+
+
+/**
+ * The task manager factory for a null writer.
+ * 
+ * @author Brett Henderson
+ */
+public class NullWriterFactory extends TaskManagerFactory {
+	
+	/**
+	 * Creates a new instance and adds the class to the global register.
+	 * 
+	 * @param taskType
+	 *            The name to register the type against.
+	 */
+	public NullWriterFactory(String taskType) {
+		super(taskType);
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected TaskManager createTaskManagerImpl(String taskId, Map<String, String> taskArgs, Map<String, String> pipeArgs) {
+		return new SinkManager(taskId, new NullWriter(), pipeArgs);
+	}
+}
