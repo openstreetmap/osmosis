f790dd5cab71e14c2fa864b9110db15c332e0e45
Brett Henderson <brett@bretth.com>
2007-07-03 12:22:21 +0000

    Increment version to 0.1.8.

--- src/com/bretth/osmosis/Osmosis.java
+++ src/com/bretth/osmosis/Osmosis.java
@@ -14,7 +14,7 @@ import com.bretth.osmosis.pipeline.Pipeline;
  * @author Brett Henderson
  */
 public class Osmosis {
-	private static final String VERSION = "0.1.7";
+	private static final String VERSION = "0.1.8";
 	
 	private static final Logger log = Logger.getLogger(Osmosis.class.getName());
 	
