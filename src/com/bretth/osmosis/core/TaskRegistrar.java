1247431eeebd5251af8d1763286c57ad78ac202c
Brett Henderson <brett@bretth.com>
2007-09-13 08:08:32 +0000

    Exposed 0.5 xml tasks to command line via TaskRegistrar.

--- src/com/bretth/osmosis/core/TaskRegistrar.java
+++ src/com/bretth/osmosis/core/TaskRegistrar.java
@@ -107,22 +107,22 @@ public class TaskRegistrar {
 		TaskManagerFactory.register("read-mysql-0.5", new com.bretth.osmosis.core.mysql.v0_5.MysqlReaderFactory());
 		TaskManagerFactory.register("read-mysql-change-0.5", new com.bretth.osmosis.core.mysql.v0_5.MysqlChangeReaderFactory());
 		TaskManagerFactory.register("read-mysql-current-0.5", new com.bretth.osmosis.core.mysql.v0_5.MySqlCurrentReaderFactory());
-		//TaskManagerFactory.register("read-xml-0.5", new XmlReaderFactory());
-		//TaskManagerFactory.register("read-xml-change-0.5", new XmlChangeReaderFactory());
+		TaskManagerFactory.register("read-xml-0.5", new com.bretth.osmosis.core.xml.v0_5.XmlReaderFactory());
+		TaskManagerFactory.register("read-xml-change-0.5", new com.bretth.osmosis.core.xml.v0_5.XmlChangeReaderFactory());
 		//TaskManagerFactory.register("sort-0.5", entitySorterFactory);
-		TaskManagerFactory.register("sort-change-0.5", changeSorterFactory);
+		//TaskManagerFactory.register("sort-change-0.5", changeSorterFactory);
 		TaskManagerFactory.register("write-mysql-0.5", new com.bretth.osmosis.core.mysql.v0_5.MysqlWriterFactory());
 		TaskManagerFactory.register("write-mysql-change-0.5", new com.bretth.osmosis.core.mysql.v0_5.MysqlChangeWriterFactory());
 		TaskManagerFactory.register("truncate-mysql-0.5", new com.bretth.osmosis.core.mysql.v0_5.MysqlTruncatorFactory());
-		//TaskManagerFactory.register("write-xml-0.5", new XmlWriterFactory());
-		//TaskManagerFactory.register("write-xml-change-0.5", new XmlChangeWriterFactory());
+		TaskManagerFactory.register("write-xml-0.5", new com.bretth.osmosis.core.xml.v0_5.XmlWriterFactory());
+		TaskManagerFactory.register("write-xml-change-0.5", new com.bretth.osmosis.core.xml.v0_5.XmlChangeWriterFactory());
 		TaskManagerFactory.register("write-null-0.5", new com.bretth.osmosis.core.misc.v0_5.NullWriterFactory());
 		TaskManagerFactory.register("write-null-change-0.5", new com.bretth.osmosis.core.misc.v0_5.NullChangeWriterFactory());
 		TaskManagerFactory.register("buffer-0.5", new com.bretth.osmosis.core.buffer.v0_5.EntityBufferFactory());
 		TaskManagerFactory.register("buffer-change-0.5", new com.bretth.osmosis.core.buffer.v0_5.ChangeBufferFactory());
 		TaskManagerFactory.register("merge-0.5", new com.bretth.osmosis.core.merge.v0_5.EntityMergerFactory());
 		TaskManagerFactory.register("merge-change-0.5", new com.bretth.osmosis.core.merge.v0_5.ChangeMergerFactory());
-		//TaskManagerFactory.register("read-api-0.5", new XmlDownloaderFactory());
+		TaskManagerFactory.register("read-api-0.5", new com.bretth.osmosis.core.xml.v0_5.XmlDownloaderFactory());
 		TaskManagerFactory.register("bounding-polygon-0.5", new com.bretth.osmosis.core.filter.v0_5.PolygonFilterFactory());
 		//TaskManagerFactory.register("report-0.5", new EntityReporterFactory());
 	}
