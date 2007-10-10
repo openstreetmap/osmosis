3caf996ebfb3f475b6ffc6e6e87e729d034f4c7a
Brett Henderson <brett@bretth.com>
2007-10-10 10:38:37 +0000

    Updated the build script to better support unit tests.  No tests available yet.

---
+++ test/com/bretth/osmosis/core/xml/v0_5/XmlChangeWriterTest.java
@@ -0,0 +1,38 @@
+package com.bretth.osmosis.core.xml.v0_5;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class XmlChangeWriterTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void testProcess() {
+		fail("Not yet implemented");
+	}
+}
