d56a24bd043a5c6e8fa8c8c4b718e6fcf3544764
Brett Henderson <brett@bretth.com>
2008-03-31 10:14:11 +0000

    Added comments to the pgsql simple schema scripts.

--- script/pgsql_simple_load.sql
+++ script/pgsql_simple_load.sql
@@ -1,3 +1,4 @@
+-- Drop all primary keys and indexes to improve load speed.
 ALTER TABLE node DROP CONSTRAINT pk_node;
 ALTER TABLE way DROP CONSTRAINT pk_way;
 ALTER TABLE way_node DROP CONSTRAINT pk_way_node;
@@ -8,6 +9,7 @@ DROP INDEX idx_way_tag_way_id;
 DROP INDEX idx_relation_tag_relation_id;
 DROP INDEX idx_way_bbox;
 
+-- Import the table data from the data files using the fast COPY method.
 COPY node FROM E'C:\\tmp\\pgimport\\node.txt';
 COPY node_tag FROM E'C:\\tmp\\pgimport\\node_tag.txt';
 COPY way FROM E'C:\\tmp\\pgimport\\way.txt';
@@ -17,6 +19,7 @@ COPY relation FROM E'C:\\tmp\\pgimport\\relation.txt';
 COPY relation_tag FROM E'C:\\tmp\\pgimport\\relation_tag.txt';
 COPY relation_member FROM E'C:\\tmp\\pgimport\\relation_member.txt';
 
+-- Add the primary keys and indexes back again (except the way bbox index).
 ALTER TABLE ONLY node ADD CONSTRAINT pk_node PRIMARY KEY (id);
 ALTER TABLE ONLY way ADD CONSTRAINT pk_way PRIMARY KEY (id);
 ALTER TABLE ONLY way_node ADD CONSTRAINT pk_way_node PRIMARY KEY (way_id, sequence_id);
@@ -26,13 +29,16 @@ CREATE INDEX idx_node_location ON node USING gist (coordinate);
 CREATE INDEX idx_way_tag_way_id ON way_tag USING btree (way_id);
 CREATE INDEX idx_relation_tag_relation_id ON relation_tag USING btree (relation_id);
 
+-- Update the bbox column of the way table.
 UPDATE way SET bbox = (
 	SELECT Envelope(Collect(coordinate))
 	FROM node JOIN way_node ON way_node.node_id = node.id
 	WHERE way_node.way_id = way.id
 );
 
+-- Index the way bounding box column.
 CREATE INDEX idx_way_bbox ON way USING gist (bbox);
 
+-- Perform database maintenance due to large database changes.
 VACUUM;
 ANALYZE;
--- script/pgsql_simple_schema.sql
+++ script/pgsql_simple_schema.sql
@@ -1,4 +1,6 @@
 -- Database creation script for the simple PostgreSQL schema.
+
+-- Drop all tables if they exist.
 DROP TABLE IF EXISTS node;
 DROP TABLE IF EXISTS node_tag;
 DROP TABLE IF EXISTS way;
@@ -10,18 +12,22 @@ DROP TABLE IF EXISTS relation_tag;
 DROP TABLE IF EXISTS schema_info;
 
 
+-- Create a table which will contain a single row defining the current schema version.
 CREATE TABLE schema_info (
     version integer NOT NULL
 );
 
 
+-- Create a table for nodes.
 CREATE TABLE node (
     id bigint NOT NULL,
     user_name text NOT NULL,
     tstamp timestamp without time zone NOT NULL
 );
+-- Add a postgis point column holding the location of the node.
 SELECT AddGeometryColumn('node', 'coordinate', -1, 'POINT', 2);
 
+-- Create a table for node tags.
 CREATE TABLE node_tag (
     node_id bigint NOT NULL,
     name text NOT NULL,
@@ -29,14 +35,18 @@ CREATE TABLE node_tag (
 );
 
 
+-- Create a table for ways.
 CREATE TABLE way (
     id bigint NOT NULL,
     user_name text NOT NULL,
     tstamp timestamp without time zone NOT NULL
 );
+-- Add a postgis bounding box column used for indexing the location of the way.
+-- This will contain a bounding box surrounding the extremities of the way.
 SELECT AddGeometryColumn('way', 'bbox', -1, 'GEOMETRY', 2);
 
 
+-- Create a table for representing way to node relationships.
 CREATE TABLE way_node (
     way_id bigint NOT NULL,
     node_id bigint NOT NULL,
@@ -44,6 +54,7 @@ CREATE TABLE way_node (
 );
 
 
+-- Create a table for way tags.
 CREATE TABLE way_tag (
     way_id bigint NOT NULL,
     name text NOT NULL,
@@ -51,13 +62,14 @@ CREATE TABLE way_tag (
 );
 
 
+-- Create a table for relations.
 CREATE TABLE relation (
     id bigint NOT NULL,
     user_name text NOT NULL,
     tstamp timestamp without time zone NOT NULL
 );
 
-
+-- Create a table for representing relation member relationships.
 CREATE TABLE relation_member (
     relation_id bigint NOT NULL,
     member_id bigint NOT NULL,
@@ -66,6 +78,7 @@ CREATE TABLE relation_member (
 );
 
 
+-- Create a table for relation tags.
 CREATE TABLE relation_tag (
     relation_id bigint NOT NULL,
     name text NOT NULL,
@@ -73,8 +86,11 @@ CREATE TABLE relation_tag (
 );
 
 
+-- Configure the schema version.
 INSERT INTO schema_info (version) VALUES (1);
 
+
+-- Add primary keys to tables.
 
 ALTER TABLE ONLY schema_info ADD CONSTRAINT pk_schema_info PRIMARY KEY (version);
 
@@ -90,6 +106,8 @@ ALTER TABLE ONLY way_node ADD CONSTRAINT pk_way_node PRIMARY KEY (way_id, sequen
 
 ALTER TABLE ONLY relation ADD CONSTRAINT pk_relation PRIMARY KEY (id);
 
+
+-- Add indexes to tables.
 
 CREATE INDEX idx_node_tag_node_id ON node_tag USING btree (node_id);
 CREATE INDEX idx_node_location ON node USING gist (coordinate);
