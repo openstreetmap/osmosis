279a6b273bd9833f2831a2f9b05eeea4165ad82f
Brett Henderson <brett@bretth.com>
2008-05-26 12:44:49 +0000

    Updated the version to 0.27.1.

--- src/com/bretth/osmosis/core/OsmosisConstants.java
+++ src/com/bretth/osmosis/core/OsmosisConstants.java
@@ -10,5 +10,5 @@ public interface OsmosisConstants {
 	/**
 	 * Defines the version of the Osmosis application.
 	 */
-	static final String VERSION = "0.27";
+	static final String VERSION = "0.27.1";
 }
