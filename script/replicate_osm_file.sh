a6d0ee8df8e96229baaed8473c5f8c4b301cfb3a
Brett Henderson <brett@bretth.com>
2008-05-27 12:11:28 +0000

    Added a simple shell script for maintaining an up-to-date osm file locally.

---
+++ script/replicate_osm_file.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This script automates the replication of changes into an offline osm file for a specific area of interest.  This allows an up-to-date local snapshot of an area to be maintained.
+
+# The name of the replicated file.
+OSM_FILE = myfile.osm.gz
+
+# The name of the temp file to create during processing (Note: must have the same extension as OSM_FILE to ensure the same compression method is used.
+TEMP_OSM_FILE = tmp.osm.gz
+
+# The directory containing the state associated with the --read-change-interval task previously initiated with the --read-change-interval-init task.
+WORKING_DIRECTORY = ./
+
+# The bounding box to maintain.
+LEFT = -180
+BOTTOM = -90
+RIGHT = 180
+TOP = 90
+
+# The osmosis command.
+CMD = osmosis -q
+
+if [ ! $CMD -q --read-change-interval $WORKING_DIRECTORY --read-xml $OSM_FILE --apply-change --bbox left=$LEFT bottom=$BOTTOM right=$RIGHT top=$TOP --write-xml $TEMP_OSM_FILE ]; then
+	
+	exit -1
+	
+fi
+
+mv $TEMP_OSM_FILE $OSM_FILE
