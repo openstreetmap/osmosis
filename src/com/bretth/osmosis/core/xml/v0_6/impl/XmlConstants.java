f910ba6b319d341d38f0f85fd1f4c9547d786ed7
Karl Newman <siliconfiend@gmail.com>
2008-07-30 17:46:41 +0000

    Osmosis: Update XML constants for 0.6.

--- src/com/bretth/osmosis/core/xml/v0_6/impl/XmlConstants.java
+++ src/com/bretth/osmosis/core/xml/v0_6/impl/XmlConstants.java
@@ -12,11 +12,11 @@ public interface XmlConstants {
 	 * Defines the version number to be stored in osm xml files. This number
 	 * will also be applied to osmChange files.
 	 */
-	public static final String OSM_VERSION = "0.5";
+	public static final String OSM_VERSION = "0.6";
 	
 	
 	/**
 	 * The default URL for the production API.
 	 */
-	public static final String DEFAULT_URL = "http://www.openstreetmap.org/api/0.5";
+	public static final String DEFAULT_URL = "http://www.openstreetmap.org/api/0.6";
 }
