72e9486162aa863fe4a37322817bd8fa65073a79
Brett Henderson <brett@bretth.com>
2008-08-14 13:33:24 +0000

    Begun adding a simple xml reader and writer test.

---
+++ test/com/bretth/osmosis/core/xml/v0_6/XmlReaderWriterTest.java
@@ -0,0 +1,25 @@
+package com.bretth.osmosis.core.xml.v0_6;
+
+import org.junit.Test;
+
+/**
+ * A simple test verifying the operation of the xml reader and writer tasks.
+ * 
+ * @author Brett Henderson
+ */
+public class XmlReaderWriterTest {
+	/**
+	 * A basic test reading and writing an osm file testing both reader and writer tasks.
+	 */
+	@Test
+	public void testSimple() {
+		//XmlReader xmlReader;
+		//URL url;
+		
+		//xmlReader = new XmlReader(this.getClass().getResource("/data/v0_6/xml-task-tests-v0_6.osm").openConnection().);
+		//url = getClass().getResource("/data/v0_6/xml-task-tests-v0_6.osm");
+		//File file = new File(url.getFile());
+		//url.toString();
+		//file.toString();
+	}
+}
---
+++ test/data/v0_6/readme.txt
@@ -0,0 +1,3 @@
+The following substitutions are performed on files in this directory.
+
+%VERSION% - Replaced with the current osmosis version, useful for updating xml files.
