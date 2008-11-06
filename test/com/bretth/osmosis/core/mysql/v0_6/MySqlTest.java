1f1d13bef4484b72ed3083562cdceac81eafff2d
Brett Henderson <brett@bretth.com>
2008-11-06 12:45:29 +0000

    Added a new mysql test for the changeset extract code.

--- test/com/bretth/osmosis/core/mysql/v0_6/MySqlTest.java
+++ test/com/bretth/osmosis/core/mysql/v0_6/MySqlTest.java
@@ -168,7 +168,7 @@ public class MySqlTest {
 	 *             if any file operations fail.
 	 */
 	@Test
-	public void testChangeset() throws IOException {
+	public void testApplyChangeset() throws IOException {
 		File authFile;
 		File snapshotFile;
 		File changesetFile;
@@ -300,6 +300,83 @@ public class MySqlTest {
 				"--tag-sort-0.6",
 				"--member-sort-0.6",
 				"--write-xml-0.6",
+				actualResultFile.getPath()
+			}
+		);
+		
+		// Validate that the dumped file matches the expected result.
+		fileUtils.compareFiles(expectedResultFile, actualResultFile);
+		
+		// Success so delete the output file.
+		actualResultFile.delete();
+	}
+
+
+	/**
+	 * A test loading an osm file into a mysql database, then applying a
+	 * changeset, then extracting the changeset timeframe and verifying the
+	 * output is as expected.
+	 * 
+	 * @throws IOException
+	 *             if any file operations fail.
+	 * @throws ParseException
+	 *             if any date operations fail.
+	 */
+	@Test
+	public void testChangesetDump() throws IOException, ParseException {
+		File authFile;
+		File snapshotFile;
+		File changesetFile;
+		File expectedResultFile;
+		File actualResultFile;
+		
+		// Generate input files.
+		authFile = fileUtils.getDataFile("v0_6/mysql-authfile.txt");
+		snapshotFile = fileUtils.getDataFile("v0_6/db-snapshot.osm");
+		changesetFile = fileUtils.getDataFile("v0_6/db-changeset.osc");
+		expectedResultFile = fileUtils.getDataFile("v0_6/db-changeset-b.osc");
+		actualResultFile = File.createTempFile("test", ".osm");
+		
+		// Remove all existing data from the database.
+		Osmosis.run(
+			new String [] {
+				"-q",
+				"--truncate-mysql-0.6",
+				"authFile=" + authFile.getPath()
+			}
+		);
+		
+		// Load the database with the snapshot file.
+		Osmosis.run(
+			new String [] {
+				"-q",
+				"--read-xml-0.6",
+				snapshotFile.getPath(),
+				"--write-mysql-0.6",
+				"authFile=" + authFile.getPath()
+			}
+		);
+		
+		// Apply the changeset file to the database.
+		Osmosis.run(
+			new String [] {
+				"-q",
+				"--read-xml-change-0.6",
+				changesetFile.getPath(),
+				"--write-mysql-change-0.6",
+				"authFile=" + authFile.getPath()
+			}
+		);
+		
+		// Dump the database to an osm file.
+		Osmosis.run(
+			new String [] {
+				"-q",
+				"--read-mysql-change-0.6",
+				"intervalBegin=" + convertUTCTimeToLocalTime("2008-01-03_00:00:00"),
+				"intervalEnd=" + convertUTCTimeToLocalTime("2008-01-04_00:00:00"),
+				"authFile=" + authFile.getPath(),
+				"--write-xml-change-0.6",
 				actualResultFile.getPath()
 			}
 		);
