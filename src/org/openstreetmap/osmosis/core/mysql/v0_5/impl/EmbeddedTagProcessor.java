1a2da5dc4ec5b5c59d328a618d5ae3226d27ab2a
Brett Henderson <brett@bretth.com>
2009-02-04 11:18:28 +0000

    Added proper escaping support to the mysql embedded tag processing so that multi-value tags with ';' characters will be supported correctly.

--- src/org/openstreetmap/osmosis/core/mysql/v0_5/impl/EmbeddedTagProcessor.java
+++ src/org/openstreetmap/osmosis/core/mysql/v0_5/impl/EmbeddedTagProcessor.java
@@ -4,6 +4,8 @@ package org.openstreetmap.osmosis.core.mysql.v0_5.impl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.openstreetmap.osmosis.core.domain.v0_5.Tag;
 
@@ -16,6 +18,27 @@ import org.openstreetmap.osmosis.core.domain.v0_5.Tag;
  */
 public class EmbeddedTagProcessor {
 	
+	private Pattern slashEscapePattern;
+	private Pattern semiColonEscapePattern;
+	private Pattern slashUnescapePattern;
+	private String slashEscapeReplacement;
+	private String semiColonEscapeReplacement;
+	private String slashUnescapeReplacement;
+	
+	
+	/**
+	 * Creates a new instance.
+	 */
+	public EmbeddedTagProcessor() {
+		slashEscapePattern = Pattern.compile("\\", Pattern.LITERAL);
+		semiColonEscapePattern = Pattern.compile(";");
+		slashUnescapePattern = Pattern.compile("\\\\", Pattern.LITERAL);
+		slashEscapeReplacement = Matcher.quoteReplacement("\\\\");
+		semiColonEscapeReplacement = Matcher.quoteReplacement("\\;");
+		slashUnescapeReplacement = Matcher.quoteReplacement("\\");
+	}
+	
+	
 	/**
 	 * Parses the specified tag string and produces corresponding tag objects.
 	 * 
@@ -26,35 +49,71 @@ public class EmbeddedTagProcessor {
 	public List<Tag> parseTags(String tags) {
 		StringTokenizer tokenizer;
 		List<Tag> tagList;
+		StringBuilder token;
 		
 		tagList = new ArrayList<Tag>();
 		
 		tokenizer = new StringTokenizer(tags, ";");
+		token = new StringBuilder();
 		
 		while (tokenizer.hasMoreTokens()) {
-			String token;
 			String key;
 			String value;
 			int equalsIndex;
+			String unescapedToken;
 			
-			token = tokenizer.nextToken();
+			// Append the next token to the token buffer.
+			token.append(tokenizer.nextToken());
 			
-			equalsIndex = token.indexOf("=");
+			// If the current token ends with '\' and there are more tokens then
+			// the ';' wasn't a token separator, it was escaped.
+			if (token.charAt(token.length() - 1) == '\\' && tokenizer.hasMoreTokens()) {
+				// Replace the '\' that was read with the ';' which was thought
+				// to be a separator by the tokenizer.
+				token.setCharAt(token.length() - 1, ';');
+				
+				// Begin at the start of the loop again.
+				continue;
+			}
 			
+			// Replace the escaped '\' sequences (ie. "\\" with a single '\').
+			unescapedToken = slashUnescapePattern.matcher(token.toString()).replaceAll(slashUnescapeReplacement);
+			
+			// Find the location of the '=' character separating the tag key and value.
+			equalsIndex = unescapedToken.indexOf("=");
+			
+			// If the '=' is not found, the entire token is the key and the value is empty.
 			if (equalsIndex > 0) {
-				key = token.substring(0, equalsIndex);
-				value = token.substring(equalsIndex + 1, token.length());
+				key = unescapedToken.substring(0, equalsIndex);
+				value = unescapedToken.substring(equalsIndex + 1, unescapedToken.length());
 			} else {
-				key = token;
+				key = unescapedToken;
 				value = "";
 			}
 			
+			// Add the tag to the list.
 			tagList.add(
 				new Tag(key, value)
 			);
+			// Reset the tag data buffer.
+			token.setLength(0);
 		}
 		
 		return tagList;
+	}
+	
+	
+	private String escapeValue(String value) {
+		String escapedValue;
+		
+		escapedValue = value;
+		
+		// Replace "\" with "\\".
+		escapedValue = slashEscapePattern.matcher(escapedValue).replaceAll(slashEscapeReplacement);
+		// Replace ";" with "\;".
+		escapedValue = semiColonEscapePattern.matcher(escapedValue).replaceAll(semiColonEscapeReplacement);
+		
+		return escapedValue;
 	}
 	
 	
@@ -74,7 +133,7 @@ public class EmbeddedTagProcessor {
 				tagBuffer.append(';');
 			}
 			
-			tagBuffer.append(tag.getKey()).append("=").append(tag.getValue());
+			tagBuffer.append(tag.getKey()).append("=").append(escapeValue(tag.getValue()));
 		}
 		
 		return tagBuffer.toString();
