ff09be859fe61e219978497b0070774266a63327
Brett Henderson <brett@bretth.com>
2009-02-26 04:31:23 +0000

    Modified the change deriver task to use the new modifiable entities instead of the builder classes.

---
+++ src/org/openstreetmap/osmosis/core/change/v0_6/impl/TimestampSetter.java
@@ -0,0 +1,47 @@
+// License: GPL. Copyright 2007-2008 by Brett Henderson and other contributors.
+package org.openstreetmap.osmosis.core.change.v0_6.impl;
+
+import java.util.Calendar;
+
+import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
+import org.openstreetmap.osmosis.core.domain.common.SimpleTimestampContainer;
+import org.openstreetmap.osmosis.core.domain.common.TimestampContainer;
+
+
+/**
+ * Updates the current timestamp on to entities. Note that the same time will be applied to all
+ * entities and will be the time that the internal timestamp was first derived.
+ */
+public class TimestampSetter {
+	private TimestampContainer timestamp;
+	
+	
+	/**
+	 * Creates a new instance.
+	 */
+	public TimestampSetter() {
+		Calendar calendar;
+		
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.MILLISECOND, 0);
+		timestamp = new SimpleTimestampContainer(calendar.getTime());
+	}
+	
+	
+	/**
+	 * Updates the timestamp on the supplied entity. A new entity container may be created if the
+	 * existing one is read-only.
+	 * 
+	 * @param entityContainer
+	 *            The container holding the entity to be modified.
+	 * @return A container holding an updated entity.
+	 */
+	public EntityContainer updateTimestamp(EntityContainer entityContainer) {
+		EntityContainer resultContainer;
+		
+		resultContainer = entityContainer.getWriteableInstance();
+		resultContainer.getEntity().setTimestampContainer(timestamp);
+		
+		return resultContainer;
+	}
+}
