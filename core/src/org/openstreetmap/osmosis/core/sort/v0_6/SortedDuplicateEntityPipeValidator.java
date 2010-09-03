ad3d3e2d8d7e3f9099a2787fbb1e194ee79816f8
Brett Henderson <brett@bretth.com>
2010-09-03 12:27:38 +0000

    Applied the FlattenFilter patch provided by Andrzej Zaborowski and modified it to use a new SortedDuplicateEntityPipeValidator.

---
+++ core/src/org/openstreetmap/osmosis/core/sort/v0_6/SortedDuplicateEntityPipeValidator.java
@@ -0,0 +1,79 @@
+// This software is released into the Public Domain.  See copying.txt for details.
+package org.openstreetmap.osmosis.core.sort.v0_6;
+
+import org.openstreetmap.osmosis.core.OsmosisRuntimeException;
+import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
+import org.openstreetmap.osmosis.core.task.v0_6.Sink;
+import org.openstreetmap.osmosis.core.task.v0_6.SinkSource;
+
+
+/**
+ * Validates that entity data in a pipeline is sorted by entity type then id. It
+ * allows duplicates. It accepts input data from a Source and passes all data to
+ * a downstream Sink.
+ * 
+ * @author Brett Henderson
+ */
+public class SortedDuplicateEntityPipeValidator implements SinkSource {
+	private Sink sink;
+	private EntityContainerComparator comparator;
+	private EntityContainer previousEntityContainer;
+	
+	
+	/**
+	 * Creates a new instance.
+	 */
+	public SortedDuplicateEntityPipeValidator() {
+		comparator = new EntityContainerComparator(new EntityByTypeThenIdComparator());
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void complete() {
+		sink.complete();
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void process(EntityContainer entityContainer) {
+		// If this is not the first entity in the pipeline, make sure this
+		// entity is greater than the previous.
+		if (previousEntityContainer != null) {
+			if (comparator.compare(previousEntityContainer,	entityContainer) > 0) {
+				throw new OsmosisRuntimeException(
+					"Pipeline entities are not sorted, previous entity type="
+					+ previousEntityContainer.getEntity().getType() + ", id="
+					+ previousEntityContainer.getEntity().getId() + ", version="
+					+ previousEntityContainer.getEntity().getVersion() + " current entity type="
+					+ entityContainer.getEntity().getType() + ", id=" 
+                    + entityContainer.getEntity().getId() + ", version="
+                    + entityContainer.getEntity().getVersion() + "."
+				);
+			}
+		}
+		
+		sink.process(entityContainer);
+		
+		previousEntityContainer = entityContainer;
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void release() {
+		sink.release();
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setSink(Sink sink) {
+		this.sink = sink;
+	}
+}
--- set/src/org/openstreetmap/osmosis/set/v0_6/FlattenFilter.java
+++ set/src/org/openstreetmap/osmosis/set/v0_6/FlattenFilter.java
@@ -5,52 +5,76 @@ import org.openstreetmap.osmosis.core.container.v0_6.BoundContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
 import org.openstreetmap.osmosis.core.domain.v0_6.Entity;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
-import org.openstreetmap.osmosis.core.task.v0_6.SinkSource;
+import org.openstreetmap.osmosis.core.sort.v0_6.SortedDuplicateEntityPipeValidator;
 
 
 /**
- * Flatten / simplify a sorted entity stream.
- * (similar to --simplify-change)
+ * Flatten / simplify a sorted entity stream. (similar to --simplify-change)
  */
-public class FlattenFilter implements SinkSource {
+public class FlattenFilter extends SortedDuplicateEntityPipeValidator {
 	private Sink sink;
 	private EntityContainer previousContainer;
+	private Sink flattener = new Sink() {
+		/**
+		 * Process a node, way or relation.
+		 * 
+		 * @param currentContainer
+		 *            The entity container to be processed.
+		 */
+		@Override
+		public void process(EntityContainer currentContainer) {
+			if (previousContainer == null) {
+				previousContainer = currentContainer;
+				return;
+			}
+
+			Entity current = currentContainer.getEntity();
+			Entity previous = previousContainer.getEntity();
+
+			if (current.getId() != previous.getId() || !current.getType().equals(previous.getType())) {
+				sink.process(previousContainer);
+				previousContainer = currentContainer;
+				return;
+			}
+
+			if (current.getVersion() > previous.getVersion()) {
+				previousContainer = currentContainer;
+			}
+		}
+
+
+		@Override
+		public void complete() {
+			/*
+			 * If we've stored entities temporarily, we now need to forward the
+			 * stored ones to the output.
+			 */
+			if (previousContainer != null) {
+				sink.process(previousContainer);
+			}
+
+			sink.complete();
+		}
+
+
+		@Override
+		public void release() {
+			sink.release();
+		}
+	};
+
 
 	/**
 	 * Creates a new instance.
 	 */
 	public FlattenFilter() {
+		super.setSink(flattener);
 	}
 
-	/**
-	 * Process a node, way or relation.
-	 *
-	 * @param currentContainer
-	 *            The entity container to be processed.
-	 */
-	public void process(EntityContainer currentContainer) {
-		if (previousContainer == null) {
-			previousContainer = currentContainer;
-			return;
-		}
-
-		Entity current = currentContainer.getEntity();
-		Entity previous = previousContainer.getEntity();
-
-		if (current.getId() != previous.getId() || !current.getType().equals(previous.getType())) {
-			sink.process(previousContainer);
-			previousContainer = currentContainer;
-			return;
-		}
-
-		if (current.getVersion() > previous.getVersion()) {
-			previousContainer = currentContainer;
-		}
-	}
 
 	/**
 	 * Process the bound.
-	 *
+	 * 
 	 * @param boundContainer
 	 *            The bound to be processed.
 	 */
@@ -59,13 +83,14 @@ public class FlattenFilter implements SinkSource {
 		sink.process(boundContainer);
 	}
 
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public void complete() {
 		/*
-		 * If we've stored entities temporarily, we now need to
-		 * forward the stored ones to the output.
+		 * If we've stored entities temporarily, we now need to forward the
+		 * stored ones to the output.
 		 */
 		if (previousContainer != null) {
 			sink.process(previousContainer);
@@ -74,12 +99,14 @@ public class FlattenFilter implements SinkSource {
 		sink.complete();
 	}
 
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public void release() {
 		sink.release();
 	}
+
 
 	/**
 	 * {@inheritDoc}
