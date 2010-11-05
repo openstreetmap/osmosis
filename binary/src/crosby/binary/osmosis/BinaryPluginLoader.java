b39a9ed9765cebbe4e9f35a790819b40f98e2d2c
Ian Dees <ian.dees@gmail.com>
2010-11-05 03:53:14 +0000

    Fixing #3325: Typo in the PBF plugin task name mapper.

--- binary/src/crosby/binary/osmosis/BinaryPluginLoader.java
+++ binary/src/crosby/binary/osmosis/BinaryPluginLoader.java
@@ -12,17 +12,20 @@ public class BinaryPluginLoader implements PluginLoader {
   @Override
   public Map<String, TaskManagerFactory> loadTaskFactories() {
           Map<String, TaskManagerFactory> factoryMap;
+
+          OsmosisReaderFactory reader = new OsmosisReaderFactory();
+          OsmosisSerializerFactory writer = new OsmosisSerializerFactory();
           
           factoryMap = new HashMap<String, TaskManagerFactory>();
-          factoryMap.put("read-pbf", new OsmosisReaderFactory());
-          factoryMap.put("read-bin", new OsmosisReaderFactory());
-          factoryMap.put("rb", new OsmosisReaderFactory());
-          factoryMap.put("write-pbf", new OsmosisSerializerFactory());
-          factoryMap.put("write-bin", new OsmosisSerializerFactory());
-          factoryMap.put("wb", new OsmosisReaderFactory());
+          factoryMap.put("read-pbf", reader);
+          factoryMap.put("read-bin", reader);
+          factoryMap.put("rb", reader);
+          factoryMap.put("write-pbf", writer);
+          factoryMap.put("write-bin", writer);
+          factoryMap.put("wb", writer);
 
-          factoryMap.put("read-pbf-0.6", new OsmosisReaderFactory());
-          factoryMap.put("write-pbf-0.6", new OsmosisSerializerFactory());
+          factoryMap.put("read-pbf-0.6", reader);
+          factoryMap.put("write-pbf-0.6", writer);
           return factoryMap;
     }
   } 
