922d817b279c2a82a085ea3cddd858851d42ba1f
Brett Henderson <brett@bretth.com>
2011-12-27 22:49:28 +1100

    Simplified sequence server threading model.
    The sequence server used to launch its own thread for managing the Netty
    runtime and sending updated sequence messages to waiting channels.  This
    added significant complexity for minimal gain because the IO is all
    asynchronous and the overhead of creating the channel messages is low.

--- replication-http/src/main/java/org/openstreetmap/osmosis/replicationhttp/v0_6/impl/SequenceServer.java
+++ replication-http/src/main/java/org/openstreetmap/osmosis/replicationhttp/v0_6/impl/SequenceServer.java
@@ -5,11 +5,8 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.Channel;
@@ -30,9 +27,6 @@ import org.openstreetmap.osmosis.core.OsmosisRuntimeException;
  * @author Brett Henderson
  */
 public class SequenceServer implements SequenceServerControl {
-	
-	private static final Logger LOG = Logger.getLogger(SequenceServer.class.getName());
-	
 
 	private int port;
 	/**
@@ -40,30 +34,12 @@ public class SequenceServer implements SequenceServerControl {
 	 */
 	private Lock sharedLock;
 	/**
-	 * This condition is used to allow the implementation thread and controlling
-	 * external thread to signal each other.
-	 */
-	private Condition controlCondition;
-	/**
 	 * A flag used only by the external control thread to remember if the server
 	 * has been started or not.
 	 */
 	private boolean masterRunning;
-	/**
-	 * A flag used to notify the server thread when it is time to stop
-	 * processing.
-	 */
-	private boolean continueRunning;
-	/**
-	 * A flag set by the server thread when it is running.
-	 */
-	private boolean threadRunning;
-	/**
-	 * A flag set by the server thread when it has initialized successfully.
-	 */
-	private boolean threadInitialized;
 	private long sequenceNumber;
-	private Thread serverThread;
+	private ChannelFactory factory;
 	private ChannelGroup allChannels;
 	private List<Channel> waitingChannels;
 
@@ -79,32 +55,10 @@ public class SequenceServer implements SequenceServerControl {
 
 		// Create the thread synchronisation primitives.
 		sharedLock = new ReentrantLock();
-		controlCondition = sharedLock.newCondition();
 
 		// Create the list of channels waiting to be notified about a new
 		// sequence.
 		waitingChannels = new ArrayList<Channel>();
-	}
-
-
-	/**
-	 * Wait for a signal on controlCondition.
-	 */
-	private void waitForUpdate() {
-		try {
-			controlCondition.await();
-
-		} catch (InterruptedException e) {
-			throw new OsmosisRuntimeException("Thread was interrupted.", e);
-		}
-	}
-
-
-	/**
-	 * Send a signal to controlCondition.
-	 */
-	private void signalUpdate() {
-		controlCondition.signal();
 	}
 
 
@@ -122,44 +76,23 @@ public class SequenceServer implements SequenceServerControl {
 				throw new OsmosisRuntimeException("The server has already been started");
 			}
 
-			// The server thread will wait during startup for this flag to
-			// become true.
-			continueRunning = false;
+			sequenceNumber = initialSequenceNumber;
 
-			// Create a new thread and launch the server.
-			serverThread = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						runServer();
-					} catch (Throwable t) {
-						LOG.log(Level.SEVERE, "Sequence server thread failed.", t);
-					}
-				}
-			}, "sequence-server-controller");
-			serverThread.start();
+			// Create a channel group to hold all channels for use during
+			// shutdown.
+			allChannels = new DefaultChannelGroup("sequence-server");
 
-			// Wait for the thread to start running. It will start but wait for
-			// masterRunning to become true before proceeding.
-			while (!threadRunning) {
-				waitForUpdate();
-			}
+			// Create the processing thread pools.
+			factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),
+					Executors.newCachedThreadPool());
 
-			// Tell the server thread to proceed with execution.
-			continueRunning = true;
-			signalUpdate();
+			// Launch the server.
+			ServerBootstrap bootstrap = new ServerBootstrap(factory);
+			bootstrap.setPipelineFactory(new SequenceServerChannelPipelineFactory(this));
+			bootstrap.setOption("child.tcpNoDelay", true);
+			bootstrap.setOption("child.keepAlive", true);
+			allChannels.add(bootstrap.bind(new InetSocketAddress(port)));
 
-			// Wait for the server to initialize.
-			while (!threadInitialized) {
-				waitForUpdate();
-
-				// If the thread is no longer running it must have failed during
-				// initialization.
-				if (!threadRunning) {
-					throw new OsmosisRuntimeException("The server failed during startup.");
-				}
-			}
-			
 			// Server startup has succeeded.
 			masterRunning = true;
 
@@ -176,19 +109,30 @@ public class SequenceServer implements SequenceServerControl {
 	 *            The new sequence number.
 	 */
 	public void update(long newSequenceNumber) {
-		if (!masterRunning) {
-			throw new OsmosisRuntimeException("The server has not been started");
-		}
-
-		// Update the sequence and notify the server thread.
 		sharedLock.lock();
 		try {
-			if (!threadRunning) {
-				throw new OsmosisRuntimeException("The server thread is no longer running.");
+			if (!masterRunning) {
+				throw new OsmosisRuntimeException("The server has not been started");
 			}
 
 			this.sequenceNumber = newSequenceNumber;
-			signalUpdate();
+
+			/*
+			 * Create a new waiting channels list and process from the original.
+			 * This is necessary because some channels may get added back in
+			 * during processing causing a concurrent modification exception.
+			 * Due to the Netty implementation, if a write operation completes
+			 * before we get a chance to register the completion listener, the
+			 * listener will run within this thread and that will mean the
+			 * channel will need to be added to the waiting list before we
+			 * complete sending messages to all the other channels.
+			 */
+			List<Channel> existingWaitingChannels = waitingChannels;
+			waitingChannels = new ArrayList<Channel>();
+			for (Channel channel : existingWaitingChannels) {
+				sendSequenceNumber(channel, sequenceNumber, true);
+			}
+
 		} finally {
 			sharedLock.unlock();
 		}
@@ -199,103 +143,17 @@ public class SequenceServer implements SequenceServerControl {
 	 * Stops the server.
 	 */
 	public void stop() {
-		if (masterRunning) {
-			sharedLock.lock();
-
-			// Notify the server thread that it is time to stop processing.
-			try {
-				// Flag the server thread to stop.
-				continueRunning = false;
-
-				signalUpdate();
-			} finally {
-				sharedLock.unlock();
-			}
-
-			// Wait for the server thread to stop.
-			try {
-				serverThread.join();
-			} catch (InterruptedException e) {
-				throw new OsmosisRuntimeException("Interrupt occurred while waiting for server thread to exit", e);
-			}
-
-			// Clear our control flag.
-			masterRunning = false;
-		}
-	}
-
-
-	/**
-	 * The main server processing method. This must be launched within a
-	 * separate thread and will run until told to shutdown.
-	 */
-	private void runServer() {
 		sharedLock.lock();
 
-		// This flag must be set for the duration of the thread execution.
-		threadRunning = true;
-		signalUpdate();
 		try {
-			// Wait for the control thread to tell us to proceed.
-			while (!continueRunning) {
-				waitForUpdate();
-			}
-
-			// Create a channel group to hold all channels for use during
-			// shutdown.
-			allChannels = new DefaultChannelGroup("sequence-server");
-
-			// Create the processing thread pools.
-			ChannelFactory factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),
-					Executors.newCachedThreadPool());
-
-			try {
-				// Launch the server.
-				ServerBootstrap bootstrap = new ServerBootstrap(factory);
-				bootstrap.setPipelineFactory(new SequenceServerChannelPipelineFactory(this));
-				bootstrap.setOption("child.tcpNoDelay", true);
-				bootstrap.setOption("child.keepAlive", true);
-				allChannels.add(bootstrap.bind(new InetSocketAddress(port)));
-				
-				// Notify the external control thread that we've initialized
-				// successfully.
-				threadInitialized = true;
-				signalUpdate();
-
-				// Process until we're told to shutdown.
-				do {
-					/*
-					 * Create a new waiting channels list and process from the
-					 * original. This is necessary because some channels may get
-					 * added back in during processing causing a concurrent
-					 * modification exception. Due to the Netty implementation,
-					 * if a write operation completes before we get a chance to
-					 * register the completion listener, the listener will run
-					 * within this thread and that will mean the channel will
-					 * need to be added to the waiting list before we complete
-					 * sending messages to all the other channels.
-					 */
-					List<Channel> existingWaitingChannels = waitingChannels;
-					waitingChannels = new ArrayList<Channel>();
-					for (Channel channel : existingWaitingChannels) {
-						sendSequenceNumber(channel, sequenceNumber, true);
-					}
-
-					waitForUpdate();
-				} while (continueRunning);
-
-			} finally {
-
-				// Shutdown the server.
+			if (masterRunning) {
 				allChannels.close().awaitUninterruptibly();
 				factory.releaseExternalResources();
-			}
 
+				// Clear our control flag.
+				masterRunning = false;
+			}
 		} finally {
-			threadRunning = false;
-			threadInitialized = false;
-			signalUpdate();
-			
 			sharedLock.unlock();
 		}
 	}
