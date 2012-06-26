09038ca511f9fa48a99083fb763b4cbe15678929
Igor Podolskiy <igor.podolskiy@vwi-stuttgart.de>
2012-06-27 00:10:19 +0200

    Add helper methods to run MultiSinkRunnableChangeSources.

--- testutil/src/main/java/org/openstreetmap/osmosis/testutil/v0_6/RunTaskUtilities.java
+++ testutil/src/main/java/org/openstreetmap/osmosis/testutil/v0_6/RunTaskUtilities.java
@@ -1,6 +1,7 @@
 // This software is released into the Public Domain.  See copying.txt for details.
 package org.openstreetmap.osmosis.testutil.v0_6;
 
+import org.openstreetmap.osmosis.core.task.v0_6.MultiSinkRunnableChangeSource;
 import org.openstreetmap.osmosis.core.task.v0_6.MultiSinkRunnableSource;
 import org.openstreetmap.osmosis.core.task.v0_6.RunnableSource;
 
@@ -52,6 +53,45 @@ public final class RunTaskUtilities {
 		return inspector;
 	}
 
+	/**
+	 * Helper method to execute a two-sink change source.
+	 * 
+	 * @param multiSink the twp-sink change source to run.
+	 * @param source1 the first source to feed to the sink.
+	 * @param source2 the second source to feed to the sink.
+	 * @return the sink change inspector containing the result.
+	 * @throws Exception if something goes wrong.
+	 */
+	public static SinkChangeInspector run(MultiSinkRunnableChangeSource multiSink, 
+			RunnableSource source1, RunnableSource source2) throws Exception {
+		return run(multiSink, source1, source2, null);
+	}
+
+	
+	/**
+	 * Helper method to execute a two-sink change source.
+	 * 
+	 * @param multiSink the twp-sink change source to run.
+	 * @param source1 the first source to feed to the sink.
+	 * @param source2 the second source to feed to the sink.
+	 * @param exceptionHandler the exception handler to attach to threads.
+	 * @return the sink change inspector containing the result.
+	 * @throws Exception if something goes wrong.
+	 */
+	public static SinkChangeInspector run(MultiSinkRunnableChangeSource multiSink, 
+			RunnableSource source1, RunnableSource source2,
+			Thread.UncaughtExceptionHandler exceptionHandler) throws Exception {
+		SinkChangeInspector inspector = new SinkChangeInspector();
+		
+		source1.setSink(multiSink.getSink(0));
+		source2.setSink(multiSink.getSink(1));
+		multiSink.setChangeSink(inspector);
+		
+		runCore(multiSink, source1, source2, exceptionHandler);
+		
+		return inspector;
+	}
+	
 	private static void runCore(Runnable multiSink,
 			Runnable source1, Runnable source2,
 			Thread.UncaughtExceptionHandler exceptionHandler)
