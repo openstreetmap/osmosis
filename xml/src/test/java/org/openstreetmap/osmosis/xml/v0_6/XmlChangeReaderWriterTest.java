5cec9bf3220b96c3e03f225dad86478e37d1daa4
Igor Podolskiy <igor.podolskiy@vwi-stuttgart.de>
2012-08-12 18:52:43 +0200

    Add some unit tests for the new lat/lon context differentiation.

--- xml/src/test/java/org/openstreetmap/osmosis/xml/v0_6/XmlChangeReaderWriterTest.java
+++ xml/src/test/java/org/openstreetmap/osmosis/xml/v0_6/XmlChangeReaderWriterTest.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.openstreetmap.osmosis.core.OsmosisRuntimeException;
+import org.openstreetmap.osmosis.core.misc.v0_6.NullChangeWriter;
 import org.openstreetmap.osmosis.testutil.AbstractDataTest;
 import org.openstreetmap.osmosis.xml.common.CompressionMethod;
 
@@ -45,4 +47,45 @@ public class XmlChangeReaderWriterTest extends AbstractDataTest {
 		// Validate that the output file matches the input file.
 		dataUtils.compareFiles(inputFile, outputFile);
 	}
+	
+	/**
+	 * Tests acceptance of nodes in a delete change with lat/lon attribute not set.
+	 * 
+	 * @throws Exception if something goes wrong.
+	 */
+	@Test
+	public void testDeleteLatLonNotSet() throws Exception {
+		XmlChangeReader xmlReader;
+		XmlChangeWriter xmlWriter;
+		File inputFile;
+		File outputFile;
+		
+		inputFile = dataUtils.createDataFile("v0_6/xml-delete-no-coordinates.osc");
+		outputFile = dataUtils.newFile();
+		
+		// Create and connect the xml tasks.
+		xmlReader = new XmlChangeReader(inputFile, true, CompressionMethod.None);
+		xmlWriter = new XmlChangeWriter(outputFile, CompressionMethod.None);
+		xmlReader.setChangeSink(xmlWriter);
+		
+		// Process the xml.
+		xmlReader.run();
+		
+		// Validate that the output file matches the input file.
+		dataUtils.compareFiles(inputFile, outputFile);
+	}
+	
+	/**
+	 * Tests non-acceptance of nodes in a non-delete change with lat/lon attribute not set.
+	 * 
+	 * @throws Exception if something goes wrong.
+	 */
+	@Test(expected = OsmosisRuntimeException.class)
+	public void testNonDeleteLatLonNotSet() throws Exception {
+		File inputFile = dataUtils.createDataFile("v0_6/xml-create-no-coordinates.osc");
+		XmlChangeReader reader = new XmlChangeReader(inputFile, false, CompressionMethod.None);
+		reader.setChangeSink(new NullChangeWriter());
+		reader.run();
+	}
+	
 }
