b8ae06f80f994a562672080029d443dec31669d2
Brett Henderson <brett@bretth.com>
2012-11-17 22:05:33 +1100

    Remove miscellaneous obsolete build files
    Remove a number of old build files that have accumulated in the tree and
    are now obsolete.  Most of these appear to be related to Maven build
    support, but this has been out of date since the move to a multi-project plugin-
    based structure.

--- build-support/hudson_pre_build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh -ex
-
-mkdir ${WORKSPACE}/trunk/src/changes
-
-${WORKSPACE}/trunk/build_support/create_changes_xml.pl < ${WORKSPACE}/trunk/changes.txt > ${WORKSPACE}/trunk/src/changes/changes.xml
