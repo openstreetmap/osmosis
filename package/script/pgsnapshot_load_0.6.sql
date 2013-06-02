ed22ce623433c7ec9f6ac34c74529bc801527e44
Paul Norman <penorman@mac.com>
2013-06-01 21:40:37 -0700

    Disable synchronous_commit for pgsnapshot loading

--- package/script/pgsnapshot_load_0.6.sql
+++ package/script/pgsnapshot_load_0.6.sql
@@ -1,3 +1,6 @@
+-- Allow data loss (but not corruption) in the case of a power outage. This is okay because we need to re-run the script anyways.
+SET synchronous_commit TO OFF;
+
 -- Drop all primary keys and indexes to improve load speed.
 ALTER TABLE nodes DROP CONSTRAINT pk_nodes;
 ALTER TABLE ways DROP CONSTRAINT pk_ways;
