fb1ac7d5747de529bb826731a516d4ae1fe479c5
Brett Henderson <brett@bretth.com>
2014-04-17 17:51:58 +1000

    Introduce AutoCloseable support
    Enhance the Osmosis Closeable interface to extend the AutoCloseable
    interface.  This will allow Osmosis Closeable resources to be used
    within JDK 1.7 try-with-resources code blocks.

--- osmosis-core/src/main/java/org/openstreetmap/osmosis/core/lifecycle/Closeable.java
+++ osmosis-core/src/main/java/org/openstreetmap/osmosis/core/lifecycle/Closeable.java
@@ -1,25 +1,24 @@
 // This software is released into the Public Domain.  See copying.txt for details.
 package org.openstreetmap.osmosis.core.lifecycle;
 
-
 /**
  * Classes that hold heavyweight resources that can't wait for garbage
  * collection should implement this interface. It provides a {@link #close()}
  * method that should be called by all clients when the class is no longer
  * required. This release method is guaranteed not to throw exceptions and
- * should always be called within a finally clause.
+ * should always be called within a finally, or try-with-resources clause.
  * 
  * @author Brett Henderson
  */
-public interface Closeable {
+public interface Closeable extends AutoCloseable {
 	/**
 	 * Performs resource cleanup tasks such as closing files, or database
 	 * connections. This must be called after all processing is complete.
 	 * Implementations should support being called multiple times, however this
 	 * is not mandatory and cannot be relied on by clients. Implementations must
 	 * call close on any nested {@link Closeable} objects. It does not throw
-	 * exceptions and should be called within a finally clause to ensure it is
-	 * called in exception scenarios.
+	 * exceptions and should be called within a finally, or try-with-resources
+	 * clause to ensure it is called in exception scenarios.
 	 */
 	void close();
 }
