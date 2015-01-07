aa4c67517afbc5b752ae34dbe95bb44c3d76b977
Brett Henderson <brett@bretth.com>
2015-01-07 16:48:49 +1100

    db-server: Add initial db server docker image for testing database tasks

---
+++ db-server/docker-start.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+DATADIR="/var/lib/pgsql/data"
+
+# test if DATADIR has content
+if [ ! "$(ls -A $DATADIR)" ]; then
+	# Create the en_US.UTF-8 locale.  We need UTF-8 support in the database.
+	localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+
+	echo "Initializing Postgres Database at $DATADIR"
+	su postgres sh -lc "initdb --encoding=UTF-8 --locale=en_US.UTF-8"
+
+	su postgres sh -lc "postgres --single -jE" <<-EOSQL
+		CREATE USER osm WITH SUPERUSER PASSWORD 'password';
+	EOSQL
+
+	# Allow the osm user to connect remotely with a password.
+	echo "host all osm 0.0.0.0/0 md5" >> "${DATADIR}/pg_hba.conf"
+
+	# Create the pgsnapshot database owned by osm.
+	su postgres sh -lc "postgres --single -jE" <<-EOSQL
+		CREATE DATABASE pgosmsnap06_test OWNER osm;
+	EOSQL
+
+	# Start the database temporarily while we configure it.
+	#su postgres sh -lc "postgres" &
+	#PID=$!
+	#echo "PID: ${PID}"
+	#sleep 1
+	su postgres sh -lc "pg_ctl -w start"
+
+	# Configure the database as the OSM user.
+	su postgres sh -lc "psql -U osm pgosmsnap06_test" <<-EOSQL
+		CREATE EXTENSION hstore;
+	EOSQL
+
+	# Stop the database.
+	su postgres sh -lc "pg_ctl -w stop"
+fi
+
+SHUTDOWN_COMMAND="echo \"Shutting down postgres\"; su postgres sh -lc \"pg_ctl -w stop\""
+trap "${SHUTDOWN_COMMAND}" SIGTERM
+trap "${SHUTDOWN_COMMAND}" SIGINT
+
+# Start the database server.
+su postgres sh -lc "pg_ctl -w start"
+
+echo "Docker container startup complete"
+
+# Wait for the server to exit.
+while test -e "/var/lib/pgsql/data/postmaster.pid"; do
+	sleep 0.5
+done
