3a382ebfae70a5db402057c18432f8f6faf363a2
Brett Henderson <brett@bretth.com>
2016-06-11 22:17:37 +1000

    db-server: Remove bretth from docker image

--- db-server/build.sh
+++ db-server/build.sh
@@ -3,4 +3,4 @@
 # Build the docker image.
 # The use of tar is a workaround for the docker restriction of not following symlinks.  This
 # tar command dereferences symlinks and then passes the resultant archive to the docker build. 
-tar -czh . | docker build -t="bretth/osmosis-build" -
+tar -czh . | docker build -t="openstreetmap.org/osmosis-db" -
