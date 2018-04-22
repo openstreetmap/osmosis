cdeacca9d35eab13f4e1d5a7b59ed897ffe55cfb
Brett Henderson <brett@bretth.com>
2018-04-22 20:54:04 +1000

    dataset: remove customdb implementation
    The customdb database was a failed attempt to implement a simple way of
    efficiently extracting bounding boxes from planet files.  It was written
    before the pgsimple and pgsnapshot implementations (if memory serves
    correctly).  It is unlikely to have been used in the wild, so I'm
    removing the code.

--- osmosis-dataset/src/main/java/org/openstreetmap/osmosis/dataset/DatasetPluginLoader.java
+++ osmosis-dataset/src/main/java/org/openstreetmap/osmosis/dataset/DatasetPluginLoader.java
@@ -1,15 +1,13 @@
 // This software is released into the Public Domain.  See copying.txt for details.
 package org.openstreetmap.osmosis.dataset;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.openstreetmap.osmosis.core.pipeline.common.TaskManagerFactory;
 import org.openstreetmap.osmosis.core.plugin.PluginLoader;
 import org.openstreetmap.osmosis.dataset.v0_6.DatasetBoundingBoxFilterFactory;
 import org.openstreetmap.osmosis.dataset.v0_6.DumpDatasetFactory;
-import org.openstreetmap.osmosis.dataset.v0_6.ReadDatasetFactory;
-import org.openstreetmap.osmosis.dataset.v0_6.WriteDatasetFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -28,18 +26,12 @@ public class DatasetPluginLoader implements PluginLoader {
 		
 		factoryMap = new HashMap<String, TaskManagerFactory>();
 		
-		factoryMap.put("write-customdb", new WriteDatasetFactory());
-		factoryMap.put("wc", new WriteDatasetFactory());
 		factoryMap.put("dataset-dump", new DumpDatasetFactory());
 		factoryMap.put("dd", new DumpDatasetFactory());
-		factoryMap.put("read-customdb", new ReadDatasetFactory());
-		factoryMap.put("rc", new ReadDatasetFactory());
 		factoryMap.put("dataset-bounding-box", new DatasetBoundingBoxFilterFactory());
 		factoryMap.put("dbb", new DatasetBoundingBoxFilterFactory());
 		
-		factoryMap.put("write-customdb-0.6", new WriteDatasetFactory());
 		factoryMap.put("dataset-dump-0.6", new DumpDatasetFactory());
-		factoryMap.put("read-customdb-0.6", new ReadDatasetFactory());
 		factoryMap.put("dataset-bounding-box-0.6", new DatasetBoundingBoxFilterFactory());
 		
 		return factoryMap;
