7f81d7b15fca5081746e6a41fc328d8f0ef81bdf
mcuthbert <mcuthbert@apple.com>
2020-03-24 10:03:51 -0700

    Created a BaseXMLReader to handle common functionality between XML Readers

--- osmosis-xml/src/main/java/org/openstreetmap/osmosis/xml/v0_6/FastXmlReader.java
+++ osmosis-xml/src/main/java/org/openstreetmap/osmosis/xml/v0_6/FastXmlReader.java
@@ -2,23 +2,22 @@
 package org.openstreetmap.osmosis.xml.v0_6;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-import org.openstreetmap.osmosis.core.OsmosisRuntimeException;
 import org.openstreetmap.osmosis.core.task.v0_6.RunnableSource;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
-import org.openstreetmap.osmosis.xml.common.CompressionActivator;
-import org.openstreetmap.osmosis.xml.common.CompressionMethod;
-import org.openstreetmap.osmosis.xml.v0_6.impl.FastXmlParser;
 
+import org.openstreetmap.osmosis.xml.common.CompressionMethod;
+import org.openstreetmap.osmosis.xml.v0_6.impl.BaseXMLReader;
+import org.openstreetmap.osmosis.xml.v0_6.impl.FastXmlParser;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * An OSM data source reading from an xml file. The entire contents of the file
@@ -27,90 +26,58 @@ import org.openstreetmap.osmosis.xml.v0_6.impl.FastXmlParser;
  * @author Jiri Clement
  * @author Brett Henderson
  */
-public class FastXmlReader implements RunnableSource {
+public class FastXmlReader extends BaseXMLReader implements RunnableSource {
+	private Sink sink;
 		
-		private static Logger log = Logger.getLogger(FastXmlReader.class.getName());
+	/**
+     * Creates a new instance.
+	 * 
+	 * @param file
+	 *            The file to read.
+	 * @param enableDateParsing
+	 *            If true, dates will be parsed from xml data, else the current
+     *            date will be used thus saving parsing time.
+     * @param compressionMethod
+	 *            Specifies the compression method to employ.
+	 */
+	public FastXmlReader(File file, boolean enableDateParsing, CompressionMethod compressionMethod) {
+        super(file, enableDateParsing, compressionMethod);
+	}
 		
-		private Sink sink;
-		private final File file;
-		private final boolean enableDateParsing;
-		private final CompressionMethod compressionMethod;
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setSink(Sink sink) {
+		this.sink = sink;
+	}
 		
-		
-		/**
-		 * Creates a new instance.
-		 * 
-		 * @param file
-		 *            The file to read.
-		 * @param enableDateParsing
-		 *            If true, dates will be parsed from xml data, else the current
-		 *            date will be used thus saving parsing time.
-		 * @param compressionMethod
-		 *            Specifies the compression method to employ.
-		 */
-		public FastXmlReader(File file, boolean enableDateParsing, CompressionMethod compressionMethod) {
-			this.file = file;
-			this.enableDateParsing = enableDateParsing;
-			this.compressionMethod = compressionMethod;
-		}
-		
-		
-		/**
-		 * {@inheritDoc}
-		 */
-		public void setSink(Sink sink) {
-			this.sink = sink;
-		}
-		
-				
-		
-		/**
-		 * Reads all data from the file and send it to the sink.
-		 */
-		public void run() {
-			InputStream inputStream = null;
-			FastXmlParser parser = null;
-			
-			try {
-				sink.initialize(Collections.<String, Object>emptyMap());
-				
-				// make "-" an alias for /dev/stdin
-				if (file.getName().equals("-")) {
-					inputStream = System.in;
-				} else {
-					inputStream = new FileInputStream(file);
-				}
-				
-				
-				inputStream =
-					new CompressionActivator(compressionMethod).
-						createCompressionInputStream(inputStream);
-				
-		        XMLInputFactory factory = XMLInputFactory.newInstance();
-		        factory.setProperty(XMLInputFactory.IS_COALESCING, false);
-		        factory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
-		        factory.setProperty(XMLInputFactory.IS_VALIDATING, false);
-		        XMLStreamReader xpp = factory.createXMLStreamReader(inputStream);
-				
-				parser = new FastXmlParser(sink, xpp, enableDateParsing);
-				
-				parser.readOsm();
-				
-				sink.complete();
-				
-			} catch (Exception e) {
-				throw new OsmosisRuntimeException("Unable to read XML file " + file + ".", e);
-			} finally {
-				sink.close();
-				
-				if (inputStream != null) {
-					try {
-						inputStream.close();
-					} catch (IOException e) {
-						log.log(Level.SEVERE, "Unable to close input stream.", e);
-					}
-					inputStream = null;
-				}
-			}
-		}
+	/**
+	 * Reads all data from the file and send it to the sink.
+	 */
+	public void run() {
+        try {
+            this.sink.initialize(Collections.emptyMap());
+            this.handleXML(null);
+            this.sink.complete();
+        } finally {
+            this.sink.close();
+        }
+	}
+
+    @Override
+    protected void parseXML(InputStream stream, DefaultHandler handler)
+                    throws SAXException, IOException {
+        try {
+            final XMLInputFactory factory = XMLInputFactory.newInstance();
+            factory.setProperty(XMLInputFactory.IS_COALESCING, false);
+            factory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
+            factory.setProperty(XMLInputFactory.IS_VALIDATING, false);
+            final XMLStreamReader xpp = factory.createXMLStreamReader(stream);
+
+            final FastXmlParser parser = new FastXmlParser(this.sink, xpp, this.isEnableDateParsing());
+            parser.readOsm();
+        } catch (final XMLStreamException e) {
+            throw new SAXException(e);
+        }
+    }
 }
--- osmosis-xml/src/main/java/org/openstreetmap/osmosis/xml/v0_6/XmlReader.java
+++ osmosis-xml/src/main/java/org/openstreetmap/osmosis/xml/v0_6/XmlReader.java
@@ -1,24 +1,15 @@
 // This software is released into the Public Domain.  See copying.txt for details.
 package org.openstreetmap.osmosis.xml.v0_6;
 
-import org.openstreetmap.osmosis.core.OsmosisRuntimeException;
 import org.openstreetmap.osmosis.core.task.v0_6.RunnableSource;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
-import org.openstreetmap.osmosis.xml.common.CompressionActivator;
-import org.openstreetmap.osmosis.xml.common.CompressionMethod;
-import org.openstreetmap.osmosis.xml.common.SaxParserFactory;
-import org.openstreetmap.osmosis.xml.v0_6.impl.OsmHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
-import javax.xml.parsers.SAXParser;
+import org.openstreetmap.osmosis.xml.common.CompressionMethod;
+import org.openstreetmap.osmosis.xml.v0_6.impl.BaseXMLReader;
+import org.openstreetmap.osmosis.xml.v0_6.impl.OsmHandler;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collections;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 /**
@@ -27,15 +18,8 @@ import java.util.logging.Logger;
  * 
  * @author Brett Henderson
  */
-public class XmlReader implements RunnableSource {
-	
-	private static Logger log = Logger.getLogger(XmlReader.class.getName());
-	
+public class XmlReader extends BaseXMLReader implements RunnableSource {
 	private Sink sink;
-	private File file;
-	private boolean enableDateParsing;
-	private CompressionMethod compressionMethod;
-	
 	
 	/**
 	 * Creates a new instance.
@@ -49,11 +33,8 @@ public class XmlReader implements RunnableSource {
 	 *            Specifies the compression method to employ.
 	 */
 	public XmlReader(File file, boolean enableDateParsing, CompressionMethod compressionMethod) {
-		this.file = file;
-		this.enableDateParsing = enableDateParsing;
-		this.compressionMethod = compressionMethod;
+        super(file, enableDateParsing, compressionMethod);
 	}
-	
 	
 	/**
 	 * {@inheritDoc}
@@ -62,58 +43,16 @@ public class XmlReader implements RunnableSource {
 		this.sink = sink;
 	}
 	
-	
 	/**
 	 * Reads all data from the file and send it to the sink.
 	 */
 	public void run() {
-		InputStream inputStream = null;
-		
 		try {
-			SAXParser parser;
-			
-			sink.initialize(Collections.<String, Object>emptyMap());
-			
-			// make "-" an alias for /dev/stdin
-			if (file.getName().equals("-")) {
-				inputStream = System.in;
-			} else {
-				inputStream = new FileInputStream(file);
-			}
-
-			inputStream =
-				new CompressionActivator(compressionMethod).
-					createCompressionInputStream(inputStream);
-
-			parser = SaxParserFactory.createParser();
-			
-			parser.parse(inputStream, new OsmHandler(sink, enableDateParsing));
-			
-			sink.complete();
-			
-		} catch (SAXParseException e) {
-			throw new OsmosisRuntimeException(
-				"Unable to parse xml file " + file
-				+ ".  publicId=(" + e.getPublicId()
-				+ "), systemId=(" + e.getSystemId()
-				+ "), lineNumber=" + e.getLineNumber()
-				+ ", columnNumber=" + e.getColumnNumber() + ".",
-				e);
-		} catch (SAXException e) {
-			throw new OsmosisRuntimeException("Unable to parse XML.", e);
-		} catch (IOException e) {
-			throw new OsmosisRuntimeException("Unable to read XML file " + file + ".", e);
+            this.sink.initialize(Collections.emptyMap());
+            this.handleXML(new OsmHandler(this.sink, this.isEnableDateParsing()));
+            this.sink.complete();
 		} finally {
-			sink.close();
-			
-			if (inputStream != null) {
-				try {
-					inputStream.close();
-				} catch (IOException e) {
-					log.log(Level.SEVERE, "Unable to close input stream.", e);
-				}
-				inputStream = null;
-			}
+            this.sink.close();
 		}
 	}
 }
